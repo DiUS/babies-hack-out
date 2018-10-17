e782ea4e576702048e2ebbbc3946d4181a6b688a
Dhiraj Chawla <dhiraj.chawla@qantas.com.au>
2018-10-17 14:01:52 +1100

    WIP

--- src/components/Train.js
+++ src/components/Train.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { Component } from 'react';
+import ml5 from 'ml5';
 
-const Train = () => (
-  <div>
-    <h2>Training section</h2>
-  </div>
-)
+class Train extends Component{
+  constructor(props) {
+    super(props);
+  }
 
-export default Train
+  async componentWillMount() {
+    const featureExtractor = await ml5.featureExtractor('MobileNet');
+    const classifier = featureExtractor.classification(video);
+    this.setState({classifier});
+  }
+
+  trainClassifier() {
+    this.state.classifier.train(function(lossValue) {
+      if (lossValue) {
+        totalLoss = lossValue;
+        loss.innerHTML = 'Loss: ' + totalLoss;
+      } else {
+        loss.innerHTML = 'Done Training! Final Loss: ' + totalLoss;
+      }
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Train the game</h2>
+        <br/>
+        <br/>
+        <button onClick={this.state.classifier.addImage('background')}>Add empty background</button>
+        <span> added</span>
+        <br/>
+        <button onClick={this.state.classifier.addImage('trumpet')}>Add Trumpet image</button>
+        <span> added</span>
+        <br/>
+        <button onClick={this.state.classifier.addImage('duck')}>Add Duck image</button>
+        <span> added</span>
+        <br/>
+        <button onClick={this.state.classifier.addImage('fork')}>Add Fork image</button>
+        <span> added</span>
+        <br/>
+        <br/>
+        <button onClick={this.trainClassifier()}>Start Training</button>
+      </div>
+    );
+  }
+
+}
+
+export default Train;
