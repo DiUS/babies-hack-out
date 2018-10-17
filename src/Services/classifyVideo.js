bf381bcb8464cb4f25f275d1935862660cc6c714
Mario Martinez <zenitram.oiram@gmail.com>
2018-10-17 13:06:38 +1100

    predict image on button click

--- src/Services/classifyVideo.js
+++ src/Services/classifyVideo.js
@@ -1,21 +1,21 @@
-import ml5 from 'ml5';
+// import ml5 from 'ml5';
 
 // Grab elements, create settings, etc.
-const video = document.getElementById('video');
+// const video = document.getElementById('video');
 
 // Create a webcam capture
-navigator.mediaDevices.getUserMedia({ video: true })
-  .then((stream) => {
-    video.srcObject = stream;
-    video.play();
-  })
+// navigator.mediaDevices.getUserMedia({ video: true })
+//   .then((stream) => {
+//     video.srcObject = stream;
+//     video.play();
+//   })
 
 // Initialize the Image Classifier method with MobileNet passing the video as the
 // second argument and the getClassification function as the third
-const classifier = ml5.imageClassifier('MobileNet', video);
+// const classifier = ml5.imageClassifier('MobileNet', video);
 
 // Needs to be called in a loop to get updated predictions
-export default async () => {
+export default async classifier => {
   const results = await classifier.predict();
   return {
     result: results[0].className,
--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,44 @@
 import React, { Component } from 'react';
+import ml5 from 'ml5';
 
 import './App.css';
 import Header from './Header';
 import Main from './Main';
 import Nav from './Nav';
 import Voice from './Voice';
+import classifyVideo from '../Services/classifyVideo';
 
 class App extends Component {
-  componentDidMount() {
+  async componentDidMount() {
     // Grab elements, create settings, etc.
     const video = document.getElementById('video');
 
     // Create a webcam capture
-    navigator.mediaDevices.getUserMedia({ video: true })
-      .then((stream) => {
-        video.srcObject = stream;
-        video.play();
-      })
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    video.srcObject = stream;
+    video.play();
+
+    const classifier = await ml5.imageClassifier('MobileNet', video);
+    this.setState({classifier});
   }
-  
+
   speak = () => {
     const text = "Hack babies hack";
 
     this.setState({text})
   }
 
+  predict = async () => {
+    const prediction = await classifyVideo(this.state.classifier);
+    this.setState({prediction});
+  }
+
   render() {
-    const {text} = this.state || {};
+    const {text, prediction} = this.state || {};
 
     return (
       <div className="App">
-        <video id="video" width="640" height="480" autoplay></video>
+        <video id="video" width="640" height="480" autoPlay></video>
         <Header />
         <Nav />
         <Main />
@@ -39,6 +47,12 @@ class App extends Component {
           Speak
         </button>
         {text && <Voice text={text} textType={"text"}/>}
+
+        <button onClick={this.predict}>
+          Predict
+        </button>
+
+        {prediction && <p>{prediction.result}, {prediction.probability}</p>}
       </div>
     );
   }
