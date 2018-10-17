04d1fd130b108c7b2507723bf1db729f81de5198
Mario Martinez <zenitram.oiram@gmail.com>
2018-10-17 13:09:08 +1100

    Clean up

--- src/Services/classifyVideo.js
+++ src/Services/classifyVideo.js
@@ -1,19 +1,3 @@
-// import ml5 from 'ml5';
-
-// Grab elements, create settings, etc.
-// const video = document.getElementById('video');
-
-// Create a webcam capture
-// navigator.mediaDevices.getUserMedia({ video: true })
-//   .then((stream) => {
-//     video.srcObject = stream;
-//     video.play();
-//   })
-
-// Initialize the Image Classifier method with MobileNet passing the video as the
-// second argument and the getClassification function as the third
-// const classifier = ml5.imageClassifier('MobileNet', video);
-
 // Needs to be called in a loop to get updated predictions
 export default async classifier => {
   const results = await classifier.predict();
